87d52c40d599481419f5190f18767476117e2036
MauroPalgi <maurolink@gmail.com>
2024-07-13 19:46:24 -0300

    init commit

---
+++ ExamenCalzado/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExamenCalzado.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
